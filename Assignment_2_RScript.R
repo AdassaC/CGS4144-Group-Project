68f9d77ebe2fc0432c6b4eaea1433a020b69f4ce
dvillalva00 <74994747+dvillalva00@users.noreply.github.com>
2022-10-02 17:19:46 -0400

    pcaPlot code added

--- Assignment_2_RScript.R
+++ Assignment_2_RScript.R
@@ -4,12 +4,37 @@ if (!("org.Hs.eg.db" %in% installed.packages())) {
   BiocManager::install("org.Hs.eg.db", update = FALSE)
 }
 
+if (!("DESeq2" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("DESeq2", update = FALSE)
+}
+if (!("EnhancedVolcano" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("EnhancedVolcano", update = FALSE)
+}
+if (!("apeglm" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("apeglm", update = FALSE)
+}
+
 # Attach the library
 library(org.Hs.eg.db)
 
 # We will need this so we can use the pipe: %>%
 library(magrittr)
 
+library(tidyverse)
+
+# Attach the DESeq2 library
+library(DESeq2)
+
+# Attach the ggplot2 library for plotting
+library(ggplot2)
+
+set.seed(12345)
+
+
+#-----Read Data-----#
 metaData <- readr::read_csv("data/metadata_file.csv")
 expression_df <- readr::read_csv("data/dataset_file.csv") %>%
   
@@ -52,38 +77,109 @@ head(mapped_df)
 # `maxsum = 10` limits the summary to 10 distinct values
 summary(as.factor(mapped_df$Symbol), maxsum = 100)
 
-library(tidyverse)
 
-my_data<-apply(expression_df[,2:70],1,log)
-plot(my_data)
+#-----Density Plot-----#
 
-#maxVals<-apply(expression_df[,2:70],1,max)
+log_scaled<-apply(expression_df[,2:70],1,log) 
+#plot(log_scaled)
 
-maxVals<-apply(my_data[,2:70],1,max)
+maxVals<-apply(log_scaled[,2:70],1,max)
 plot(maxVals)
 
-minVals<-apply(my_data[,2:70],1,min)
+minVals<-apply(log_scaled[,2:70],1,min)
 plot(minVals) 
 
+#count matrix geneXsample
 ranges <- maxVals - minVals
 d <- density(ranges) # returns the density data
 plot(d) # plots the result
 
-# data %>% ggplot(aes(x=Gene)) + geom_density()
 
-# expression_df %>%
-#   pivot_longer(cols=2:70) %>%
-#   #filter(ID == "ABCA8") %>%
-#   ggplot(aes(x=Gene, y = SRR)) +
-#   geom_point()
+#-----PlotPCA-----#
+head(metaData$Group)
+
+metaData <- metaData %>%
+  # Let's get the RPL10 mutation status from this variable
+  dplyr::mutate(cancer_status = dplyr::case_when(
+    stringr::str_detect(Group, "CTRL") ~ "normal",
+    stringr::str_detect(Group, "OC") ~ "cancerous",
+    #stringr::str_detect(Group, "Other") ~ "neither"
+    
+  ))
+
+dplyr::select(metaData, Group, cancer_status)
+
+str(metaData$cancer_status)
+
+# Make mutation_status a factor and set the levels appropriately
+metaData <- metaData %>%
+  dplyr::mutate(
+    # Here we define the values our factor variable can have and their order.
+    cancer_status = factor(cancer_status, levels = c("normal", "cancerous"))
+  )
+
+levels(metaData$cancer_status)
 
 
-#Initialize x and y
-#x <- 1:69
-#y <- 1:57736
+filtered_expression_df <- expression_df %>%
+  dplyr::filter(rowSums(.) >= 100)
 
-#Plot x and y
-#plot(x,y,)
+# round all expression counts
+gene_matrix <- round(expression_df)
 
-#Log scale
-#plot(x, y, log="y")
+ddset <- DESeqDataSetFromMatrix(
+  # Here we supply non-normalized count data
+  countData = gene_matrix,
+  # Supply the `colData` with our metadata data frame
+  colData = metaData,
+  # Supply our experimental variable to `design`
+  design = ~1 #cancer_status
+)
+
+dds_norm <- vst(ddset)
+
+plotPCA(
+  dds_norm,
+  intgroup = "Group"
+)
+
+# We first have to save the results of the `plotPCA()` function for use with `ggplot2`
+pca_results <-
+  plotPCA(
+    dds_norm,
+    intgroup = c("Group"),
+    returnData = TRUE # This argument tells R to return the PCA values
+  )
+
+
+deseq_object <- DESeq(ddset)
+
+deseq_results <- results(deseq_object)
+
+head(deseq_results)
+
+deseq_results <- lfcShrink(
+  deseq_object, # The original DESeq2 object after running DESeq()
+  coef = 3, # The log fold change coefficient used in DESeq(); the default is 2.
+  res = deseq_results # The original DESeq2 results table
+)
+
+head(deseq_results)
+
+# this is of class DESeqResults -- we want a data frame
+deseq_df <- deseq_results %>%
+  # make into data.frame
+  as.data.frame() %>%
+  # the gene names are row names -- let's make them a column for easy display
+  tibble::rownames_to_column("Gene") %>%
+  # add a column for significance threshold results
+  dplyr::mutate(threshold = pvalue < 0.05) %>%
+  # sort by statistic -- the highest values will be genes with
+  # higher expression in RPL10 mutated samples
+  dplyr::arrange(dplyr::desc(log2FoldChange))
+
+head(deseq_df)
+
+pcaPlot<-plotCounts(ddset, gene = "ENSG00000000003", intgroup = "cancer_status")
+
+pcaPlot()
